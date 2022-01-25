c01e438d92ac0bef4ff03fc16377bd464631f61c
daniela <daniela_056@hotmail.com>
2022-01-24 19:05:41 -0700

    Java data types

--- src/day2/DataTypesDemo2.java
+++ src/day2/DataTypesDemo2.java
@@ -5,8 +5,29 @@ public class DataTypesDemo2
 
 	public static void main(String[] args) 
 	{
-		byte x=10;
 
+		int a=100, b=200;
+		
+		//System.out.println(a);
+		//System.out.println(b);
+		
+		System.out.println(a+ " "+b);
+		//System.out.println(a,b); //incorrect syntax 
+
+		long c=123456789L;
+	    System.out.println(c);
+		
+		int person_age=30;
+		System.out.println(person_age);
+		
+		float item_price=15.5f; //have to specify with f character if it is a float
+		System.out.println(item_price);  // syso CTRL+space
+		
+		char grade='B';
+		System.out.println(grade);
+		
+		
+		
 	}
 
 }
