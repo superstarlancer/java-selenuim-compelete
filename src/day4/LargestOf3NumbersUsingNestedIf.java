4f5db203610b2d06479e1a7bb338692cc637f01d
daniela <daniela_056@hotmail.com>
2022-01-26 19:04:45 -0700

    Nested if else

--- src/day4/LargestOf3NumbersUsingNestedIf.java
+++ src/day4/LargestOf3NumbersUsingNestedIf.java
@@ -3,7 +3,30 @@ package day4;
 public class LargestOf3NumbersUsingNestedIf {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a=300, b=200, c=100;
+		
+		if(a>b) 
+		{
+			if(a>c) 
+			{
+				System.out.println("a is largest number" +a);
+			}
+			else 
+			{
+				System.out.println("c is largest number" +c);
+			}
+		}
+		else 
+		{
+			if(b>c) 
+			{
+				System.out.println("b is largest number" +b);
+			}
+			else 
+			{
+				System.out.println("c is largest number" +c);
+			}
+		}
 
 	}
 
