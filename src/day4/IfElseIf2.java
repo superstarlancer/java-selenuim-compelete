84bdf0b7827b380276ff578676c35dd689af310e
daniela <daniela_056@hotmail.com>
2022-01-26 18:51:50 -0700

    If else

---
+++ src/day4/IfElseIf2.java
@@ -0,0 +1,50 @@
+package day4;
+
+//Display day of the week by evaluating numbers (1...7)
+public class IfElseIf2 {
+
+	public static void main(String[] args) 
+	{
+	int weekno=1;
+	
+	if(weekno==1) 
+	{
+		System.out.println("Sunday");
+	}
+	else if(weekno==2)
+	{
+		System.out.println("Monday");
+	}
+	else if(weekno==3)
+	{
+		System.out.println("Tuesday");
+	}
+	else if(weekno==4)
+	{
+		System.out.println("Wednesday");
+	}
+	else if(weekno==5)
+	{
+		System.out.println("Thursday");
+	}
+	else if(weekno==6)
+	{
+		System.out.println("Friday");
+	}
+	else if(weekno==7)
+	{
+		System.out.println("Saturday");
+	}
+	else if(weekno==8)
+	{
+		System.out.println("Sunday");
+	}
+	else
+	{
+		System.out.println("Invalid week number");
+	}
+	
+
+	}
+
+}
---
+++ src/day4/LargestOf3NumbersUsingNestedIf.java
@@ -0,0 +1,10 @@
+package day4;
+
+public class LargestOf3NumbersUsingNestedIf {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
