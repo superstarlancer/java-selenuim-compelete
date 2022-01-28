3c9952710951b939de1cd6b21dfa68a52286f89b
daniela <daniela_056@hotmail.com>
2022-01-27 18:48:35 -0700

    While loop demo

--- src/day5/WhileloopDemo.java
+++ src/day5/WhileloopDemo.java
@@ -6,12 +6,33 @@ public class WhileloopDemo {
 	{
 	//Example1: Print 1....10 numbers i is the incrementation value
 		
-	int i =1; //initialization
+//	int i =1; //initialization
+//	
+//	while(i<=10) //condition
+//	{
+//		System.out.println(i);
+//		i++; //incrementation because if we don't i value remains the same
+//	}
 	
-	while(i<=10) //condition
+	
+	//Example3: Print even numbers between 1...10 (2,4,6,8,10)
+//	int i =2;
+//	
+//	while(i<=10)
+//	{
+//		System.out.println(i);
+//		i=i+2;
+//	}
+
+	int i=2;
+	while(i<=10) 
 	{
-		System.out.println(i);
-		i++; //incrementation because if we don't i value remains the same
+		if(i%2==0) 
+		{
+			System.out.println(i);	
+		}
+		i++;
+		
 	}
 	
 	
