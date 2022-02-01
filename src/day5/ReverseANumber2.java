1ff34d25bff7c40d64e47d299bbfbd2dee0b23d7
daniela <daniela_056@hotmail.com>
2022-01-31 22:03:49 -0700

    String Buffer

---
+++ src/day5/ReverseANumber2.java
@@ -0,0 +1,20 @@
+package day5;
+
+import java.util.Scanner;
+
+public class ReverseANumber2 {
+
+	public static void main(String[] args) {
+		//Using StringBuffer class
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter a Number:\s");
+		
+		int num=sc.nextInt();
+		StringBuffer rev;
+		StringBuffer sb=new StringBuffer(String.valueOf(num))
+		
+		System.out.println("Reverse Number is:\s"+rev);
+
+	}
+
+}
