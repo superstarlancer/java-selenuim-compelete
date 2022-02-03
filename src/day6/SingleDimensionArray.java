cb4973ec98e82e7406ce91f710eb9b00453b11a2
daniela <daniela_056@hotmail.com>
2022-02-02 19:03:41 -0700

    Two dim array

--- src/day6/SingleDimensionArray.java
+++ src/day6/SingleDimensionArray.java
@@ -29,10 +29,23 @@ public class SingleDimensionArray {
 		//4)Print all the data in the array, need a loop, specify length of array so 5 items
 		//i <=a.length-1 because we have 5 items but the index only goes to 4 so we subtract 5items -1 to get the index number or size of the array 
 		//i is the index (every value) , a is the whole array
-		for(int i=0;i<a.length-1;i++) //when it becomes 5, condition becomes false and loop stops
+		
+		/*for(int i=0;i<a.length-1;i++) //when it becomes 5, condition becomes false and loop stops
 		{
 			System.out.println(a[i]);
+		}*/
+		
+		
+		
+		//Enhanced for loop/for each loop
+		for(int x:a) //a represents all array values with one variable
+		{
+			System.out.println(x);
 		}
+		
+		
+		
+		
 
 	}
 
---
+++ src/day6/TwoDimensionalArray.java
@@ -0,0 +1,11 @@
+package day6;
+
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+		//Declaration
+		int a[][] = new int [3][2]; //3 rows 2 columns
+
+	}
+
+}
