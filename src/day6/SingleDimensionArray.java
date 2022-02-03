aee7feb3c1bded9795ef95d9259eb91b02e2382a
daniela <daniela_056@hotmail.com>
2022-02-02 18:46:16 -0700

    Arrays

--- src/day6/SingleDimensionArray.java
+++ src/day6/SingleDimensionArray.java
@@ -21,8 +21,18 @@ public class SingleDimensionArray {
 		int a[]={100,200,300,400,500};
 		
 		//2)Find size of array
-		System.out.println(a.length);
+		//System.out.println(a.length);
 		
+		//3)Print single value of an array
+		//System.out.println(a[3]); //400
+		
+		//4)Print all the data in the array, need a loop, specify length of array so 5 items
+		//i <=a.length-1 because we have 5 items but the index only goes to 4 so we subtract 5items -1 to get the index number or size of the array 
+		//i is the index (every value) , a is the whole array
+		for(int i=0;i<a.length-1;i++) //when it becomes 5, condition becomes false and loop stops
+		{
+			System.out.println(a[i]);
+		}
 
 	}
 
