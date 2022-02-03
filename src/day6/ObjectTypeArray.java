c802a84016d9a18f503392e047d2411d4bef9961
daniela <daniela_056@hotmail.com>
2022-02-02 19:54:35 -0700

    Object array

--- src/day6/ObjectTypeArray.java
+++ src/day6/ObjectTypeArray.java
@@ -3,8 +3,37 @@ package day6;
 public class ObjectTypeArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Object a[]=new Object[5];
+		
+		a[0]=100;
+		a[1]="Welcome";
+		a[2]=10.5;
+		a[3]='A';
+		a[4]=true;
+		
+		//Normal for loop
+		/*for(int i=0;i<a.length;i++) 
+		{
+			System.out.println(a[i]);
+		}*/
+		
+		
+		
+		
+		
+		
+		
+		//Enhanced Object loop
+		//need to specify what type of x it should be to hold all data types
+		//Use Object as the datatype to hold all types of data
+		for(Object x:a) 
+		{
+			System.out.println(x);
+		}
+		
+		
+		
+		
 	}
 
 }
