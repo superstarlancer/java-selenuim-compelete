a0d7f1f027334f73c40939ceccc01e5bc0a573f4
daniela <daniela_056@hotmail.com>
2022-02-02 18:30:29 -0700

    Arrays

---
+++ src/day6/SingleDimensionArray.java
@@ -0,0 +1,29 @@
+package day6;
+
+public class SingleDimensionArray {
+
+	public static void main(String[] args) 
+	{
+	
+		//1)Declare an array
+		
+		
+		//Approach 1
+		/*int a[]=new int[5]; //array declaration, specify the size of items in an array
+		
+		a[0]=100;
+		a[1]=200;
+		a[2]=300;
+		a[3]=400;
+		a[4]=500; */
+		
+		//Approach 2
+		int a[]={100,200,300,400,500};
+		
+		//2)Find size of array
+		System.out.println(a.length);
+		
+
+	}
+
+}
