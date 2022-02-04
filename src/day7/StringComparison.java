54578c69b273cb672fcdcf477f72d1264ed9cadd
daniela <daniela_056@hotmail.com>
2022-02-03 20:00:21 -0700

    omg

--- src/day7/StringComparison.java
+++ src/day7/StringComparison.java
@@ -21,6 +21,20 @@ public class StringComparison {
 		System.out.println(s1.equals(s2)); //true //COMPARES VALUES
 		
 		//== when comparing with strings vs. equals()
+		//== compares objects
+		// equal() compares values
+		
+		
+		
+		String s3=s1;
+		System.out.println(s1==s3); //true , they are the same objects because they are assigned to each other
+		System.out.println(s1.equals(s3)); //true , whatever value is in s1 is in s3
+		
+		//////////////////
+		String x="abc";
+		String y=new String("abc");
+		System.out.println(x==y); //false evaluates objects, they are not the same with the new keyword
+		System.out.println(x.equals(y)); //true
 
 	}
 
