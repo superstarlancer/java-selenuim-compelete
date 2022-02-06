08db80a0bcdedafb6776422e576bc7f80ae93ac3
daniela <daniela_056@hotmail.com>
2022-02-06 14:02:00 -0700

    Bubble Sorrt

---
+++ src/AssignmentsPart2/SortingElementsInArray.java
@@ -0,0 +1,12 @@
+package AssignmentsPart2;
+
+public class SortingElementsInArray {
+
+	public static void main(String[] args) {
+		//Bubble sort compares elements adjacent to each other and if the first element > second element we swap them,
+		
+		
+		
+	}
+
+}
