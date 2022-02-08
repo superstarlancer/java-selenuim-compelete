5ff47ca9fea1d90149895f608ecf711587a2d06d
daniela <daniela_056@hotmail.com>
2022-02-07 18:31:24 -0700

    Add employee class

---
+++ src/day8/Employee.java
@@ -0,0 +1,24 @@
+package day8;
+
+public class Employee
+{
+
+	int eid;
+	String ename;
+	String job;
+	int empsal;
+	
+	void display()
+	{
+		System.out.println(eid);
+		System.out.println(ename);
+		System.out.println(job);
+		System.out.println(empsal);
+	}
+	
+	//Need a main method
+	public static void main(String args [])
+	{
+		
+	}
+}
