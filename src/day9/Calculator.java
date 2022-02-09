d3b75a4ce3be4f6801b08999f6fc6ddb9570bda3
daniela <daniela_056@hotmail.com>
2022-02-08 18:42:33 -0700

    Calculator and methods

--- src/day9/Calculator.java
+++ src/day9/Calculator.java
@@ -5,8 +5,30 @@ public class Calculator
 	int x=10, y=20;
 	
 	//1)No Params No return value
-	void add() 
+	/*void add() 
 	{
 		System.out.println(x+y);
+	}*/
+	
+	
+	
+	//2)No Params Return value
+	//have to capture output in some other variable in main
+	//when you return something must store in variable and print that variable out
+	/*int add() 
+	{
+		return (x+y);
+	}*/
+	
+	//3)Take params No Return
+	/*void add(int a, int b)
+	{
+		System.out.println(a+b);
+	}*/
+	
+	//4)Take params Return a value
+	int add(int a, int b) 
+	{
+		return(a+b);
 	}
 }
--- src/day9/CalculatorMain.java
+++ src/day9/CalculatorMain.java
@@ -4,6 +4,21 @@ public class CalculatorMain {
 	public static void main(String[] args) 
 	{
 		Calculator cal=new Calculator();
-		cal.add();
+		//1)
+		//cal.add();
+		
+		
+		//2)
+		//int res=cal.add();
+		//System.out.println(res);
+		
+		//3)
+		//cal.add(100, 200);
+		
+		//4)
+		int res=cal.add(100, 200);
+		System.out.println(res);
+		
+		//System.out.println(cal.add(100, 200));
 	}
 }
