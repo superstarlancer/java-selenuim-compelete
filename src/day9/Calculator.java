db52520da41f0d1ca06e8d447f4ffb371a2a1f3d
daniela <daniela_056@hotmail.com>
2022-02-08 18:28:26 -0700

    Calculator

---
+++ src/day9/Calculator.java
@@ -0,0 +1,12 @@
+package day9;
+
+public class Calculator 
+{
+	int x=10, y=20;
+	
+	//1)No Params No return value
+	void add() 
+	{
+		System.out.println(x+y);
+	}
+}
---
+++ src/day9/CalculatorMain.java
@@ -0,0 +1,9 @@
+package day9;
+
+public class CalculatorMain {
+	public static void main(String[] args) 
+	{
+		Calculator cal=new Calculator();
+		cal.add();
+	}
+}
