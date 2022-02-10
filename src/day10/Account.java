8e200a26da720e36aa43ebe69ff8a10d1ca113f8
daniela <daniela_056@hotmail.com>
2022-02-09 18:19:56 -0700

    Setters & Getters

---
+++ src/day10/Account.java
@@ -0,0 +1,21 @@
+package day10;
+
+public class Account 
+{
+	private int acc_no;
+	private String name;
+	private double amount;
+	
+	void setAccNo(int ac_no) //set method assigns data into var
+	{
+		this.acc_no=ac_no;
+	}
+	
+	public int getAccNo() //get method return value from variable
+	{
+		return acc_no;
+	}
+	
+	//we'll access variables through object
+	
+}
