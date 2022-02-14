5993e8c2191112ccfbaeb8c0236ce77336b0b452
daniela <daniela_056@hotmail.com>
2022-02-13 19:04:12 -0700

    Overriding

---
+++ src/day11/OverridingDemo.java
@@ -0,0 +1,32 @@
+package day11;
+
+class Bank 
+{	
+	 double roi() 
+	 {
+		 return 0;
+	 }
+}
+
+class ICICI extends Bank 
+{
+	 double roi() 
+	 {
+		 return 10.5;
+	 }
+}
+
+class SBI extends Bank 
+{
+	
+} 
+
+
+public class OverridingDemo {
+
+	public static void main(String[] args) {
+	
+
+	}
+
+}
