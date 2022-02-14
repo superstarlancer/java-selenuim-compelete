7ee9ecf9c5b8b174970b2f008e27b6b97b13c2f3
daniela <daniela_056@hotmail.com>
2022-02-13 19:08:16 -0700

    Overriding

--- src/day11/OverridingDemo.java
+++ src/day11/OverridingDemo.java
@@ -1,5 +1,7 @@
 package day11;
 
+
+//Hierarchy Inheritance, whatever method we create in parent we can override in child by extending the class
 class Bank 
 {	
 	 double roi() 
@@ -12,21 +14,29 @@ class ICICI extends Bank
 {
 	 double roi() 
 	 {
-		 return 10.5;
+		 return 10.5; //verriding roi method
 	 }
 }
 
 class SBI extends Bank 
 {
-	
+	 double roi() 
+	 {
+		 return 9.5; //overriding roi method
+	 }	
 } 
 
 
 public class OverridingDemo {
 
-	public static void main(String[] args) {
-	
-
+	public static void main(String[] args)
+	{
+		ICICI ic=new ICICI();
+		System.out.println(ic.roi()); //10.5
+		
+		SBI sb=new SBI();
+		System.out.println(sb.roi()); //9.5
+ 
 	}
 
 }
