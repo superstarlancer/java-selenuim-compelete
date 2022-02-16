d66fb7d21185dcf4d118e8d47a7d0e9601b3b38b
daniela <daniela_056@hotmail.com>
2022-02-15 19:13:32 -0700

    Interface example

--- src/day12/interfaceconcept/InterfaceDemo.java
+++ src/day12/interfaceconcept/InterfaceDemo.java
@@ -1,9 +1,32 @@
 package day12.interfaceconcept;
 
+interface Shape 
+{
+	
+	int length=10; //final & static, don't need to specify, they are by default
+	int width=10;
+	
+	void circle(); //abstract method, all methods public by default
+	
+	default void square() //default method
+	{
+		System.out.println("I'm a Square default method...");
+	}
+	
+	static void rectangle() //static method
+	{
+		System.out.println("I'm a Rectangle static method");
+	}
+	
+	
+}
+
+
 public class InterfaceDemo {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
+		
 
 	}
 
