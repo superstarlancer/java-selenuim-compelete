9cf530c39fe7cc68fd85e48942d06be969cc9672
daniela <daniela_056@hotmail.com>
2022-02-16 19:05:34 -0700

    Data conversion classes

--- src/day13v2/DataConversionMethods.java
+++ src/day13v2/DataConversionMethods.java
@@ -3,7 +3,30 @@ package day13v2;
 public class DataConversionMethods {
 
 	public static void main(String[] args) {
+		//String --> Integer
+			/*String s="welcome"; //we cannot convert to number
+			String s1="10";
+			String s2="20";
+			System.out.println(s1+s2); //1020
+			
+			int s1num=Integer.parseInt(s1);
+			int s2num=Integer.parseInt(s2);
+			
+			System.out.println(s1num+s2num);*/
 		
+		//String --> Double
+			/*String s1="150.50";
+			String s2="110.00";
+			
+			double s1dob=Double.parseDouble(s1);
+			double s2dob=Double.parseDouble(s2);
+			
+			System.out.println(s1dob+s2dob);*/
+			
+		//String --> Boolean
+			String status="true";
+			boolean b=Boolean.parseBoolean(status);
+			System.out.println(b);
 
 	}
 
