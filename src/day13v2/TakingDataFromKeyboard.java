ad1707495ac1a0b548e0b0ec98c4026cd2fba71d
daniela <daniela_056@hotmail.com>
2022-02-16 19:35:22 -0700

    Scanner concepts

--- src/day13v2/TakingDataFromKeyboard.java
+++ src/day13v2/TakingDataFromKeyboard.java
@@ -8,9 +8,64 @@ public class TakingDataFromKeyboard {
 	{
 		
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter a number:");
-		int x=sc.nextInt();
-		System.out.println(x);
+		
+		/*System.out.println("Enter a number:");
+		int x=sc.nextInt(); //nextInt() only for integers without decimals
+		System.out.println(x+10);*/
+		
+		/*System.out.println("Enter a decimal number:");
+		double d=sc.nextDouble();
+		System.out.println(d);*/
+		
+		/*System.out.println("Enter a string value:");
+		String s=sc.next();
+		System.out.println(s.length());*/
+		
+		//Taking multiple inputs
+			/*System.out.println("Enter first number");
+			int x=sc.nextInt();
+			
+			System.out.println("Enter a second number:");
+			int y=sc.nextInt();
+			
+			System.out.println("Enter a third number");
+			int z=sc.nextInt();
+			System.out.println("Sum of three numbers is:\s"+(x+y+z));*/
+		
+		//Taking input into an array
+			/*int a[]=new int[5];
+			
+			System.out.println("Enter first value");
+			a[0]=sc.nextInt();
+			
+			System.out.println("Enter second value");
+			a[1]=sc.nextInt();
+			
+			System.out.println("Enter third value");
+			a[2]=sc.nextInt();
+			
+			System.out.println("Enter fourth value");
+			a[3]=sc.nextInt();
+			
+			System.out.println("Enter fifth value");
+			a[4]=sc.nextInt();*/
+		
+		
+		
+		//Taking many values into array by using looping statement
+		
+		int a[]=new int[10];
+		
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println("Enter Value");
+			a[i]=sc.nextInt();
+		} 
+		 //here we print the values into an array
+		for(int x:a) 
+		{
+			System.out.println(x);
+		}
 
 	}
 
