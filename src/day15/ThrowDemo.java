ae91dfc08c2eb247d0cadfcba12e3cfae00e736b
daniela <daniela_056@hotmail.com>
2022-02-21 17:48:43 -0700

    Throw demo

--- src/day15/ThrowDemo.java
+++ src/day15/ThrowDemo.java
@@ -2,34 +2,32 @@ package day15;
 
 public class ThrowDemo {
 
-	public static void main(String[] args)
+	void checksum(int num)
 	{
-		void checksum(int num) 
+		if(num<1)
 		{
-			if(num<1)
-			{
-				throw new ArithmeticException("Number is negative, cannot calculate square");
-			}
-			else 
-			{
-				System.out.println("Square of num is:"+ (num*num));
-			}
+			throw new ArithmeticException("Number is negitive, cannot calculate square..");
 		}
-		
-		
-		public static void main(String[] args) 
+		else
 		{
-			ThrowDemo td=new ThrowDemo();
-			try 
-			{
-				td.checksum(10);
-			}
-			catch(Exception e)
-			{
-				System.out.println("iii");}
-			}
-			
+			System.out.println("Squre of num is:"+ (num*num));
 		}
-
-
-}
+	}
+	
+	
+	
+	public static void main(String[] args) 
+	{
+	
+		ThrowDemo td=new ThrowDemo();
+		try
+		{
+		td.checksum(0); // invalid input
+		}
+		catch(Exception e)
+		{
+			System.out.println("invalid input provided...");
+		}
+	
+		}
+	}
