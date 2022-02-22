6b5abf4b3466d0a1b43b81ad5ec7fe5f4d1f162a
daniela <daniela_056@hotmail.com>
2022-02-21 20:00:45 -0700

    HashMap

--- src/day16/HashMapDemo.java
+++ src/day16/HashMapDemo.java
@@ -21,7 +21,28 @@ public class HashMapDemo {
 		hm.put(105,"John");
 		hm.put(101,"xyz");  //duplicate key is not allowed
 		
-		System.out.println(hm);
+		System.out.println(hm); //{101=xyz, 102=David, 103=Smith, 104=Mary, 105=John}
+		
+		//find out size
+		System.out.println(hm.size());
+		
+		//remove pair
+		hm.remove(101); //101 is key, cannot pass value, only key
+		System.out.println(hm); //print HashMap again
+		
+		//get the value from specific key
+		//104 is key, "Mary"
+		System.out.println(hm.get(104));
+		
+		//print keys only from hash
+		System.out.println(hm.keySet()); //get all keys from hashmap [102, 103, 104, 105]
+		
+		//print values only from hash
+		//method not avail, need to use a loop
+		for(Object k:hm.keySet()) 
+		{
+			System.out.println(hm.get(k));  //prints values
+		}
 	}
 
 }
