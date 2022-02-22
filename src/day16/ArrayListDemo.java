5290911e8456538e921a926d90bbff273711c714
daniela <daniela_056@hotmail.com>
2022-02-21 18:46:44 -0700

    ArrayList

---
+++ src/day16/ArrayListDemo.java
@@ -0,0 +1,43 @@
+package day16;
+
+import java.util.ArrayList;
+
+//ArrayList is a class which is implemented in List interface
+//
+//1)Heterogeneous data is allowed
+//
+//2)Insertion order —> preserved (Index)
+//
+//3)Duplicate elements- allowed
+//
+//4)Multiple nulls - allowed
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) 
+	{
+		//declaration
+		ArrayList mylist=new ArrayList();
+		//List mylist=new ArrayList();
+		
+		//ArrayList <Integer> mylist=new ArrayList<Integer>(); //only integers are allowed
+		//ArrayList <String> mylist=new ArrayList<String>();
+		
+		//Add data elements into arraylist
+		mylist.add(100);
+		mylist.add(10.5);
+		mylist.add("Welcome");
+		mylist.add('A');
+		mylist.add(true);
+		mylist.add(100);
+		mylist.add(null);
+		mylist.add(null);
+		
+		//size of arraylist
+		System.out.println(mylist.size());
+		
+		//display values from arraylist - insertion order preserved
+		System.out.println(mylist);
+	}
+
+}
