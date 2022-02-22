e52e13ee9496a3f2d7a0d81d5bf879f5450e197d
daniela <daniela_056@hotmail.com>
2022-02-21 19:53:14 -0700

    Hashmap

--- src/day16/HashMapDemo.java
+++ src/day16/HashMapDemo.java
@@ -1,10 +1,27 @@
 package day16;
 
+import java.util.HashMap;
+
 public class HashMapDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//declaration
+		//HashMap hm=new HashMap();
+		HashMap <Integer, String> hm=new HashMap<Integer, String>();
+		
+		//Map hm=new HashMap(); //Map is a parent interface of HashMap
+		
+		
+		//adding data into hashmap
+		
+		hm.put(101,"John"); //key is the first value in (key,value)
+		hm.put(102,"David");
+		hm.put(103,"Smith");
+		hm.put(104,"Mary");
+		hm.put(105,"John");
+		hm.put(101,"xyz");  //duplicate key is not allowed
+		
+		System.out.println(hm);
 	}
 
 }
