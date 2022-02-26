f1f1fe6cc3d9d1067534875832e5cfc5490ddf7e
daniela <daniela_056@hotmail.com>
2022-02-25 18:57:40 -0700

    Sliders

--- src/day20/LocatorsDemo2.java
+++ src/day20/LocatorsDemo2.java
@@ -1,6 +1,10 @@
 package day20;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LocatorsDemo2 {
@@ -12,7 +16,14 @@ public class LocatorsDemo2 {
 		
 		driver.manage().window().maximize();
 		driver.get("http://automationpractice.com/index.php");
-
+		
+		List<WebElement> sliders=driver.findElements(By.className("homeslider-container")); //able to find multiple WebElements like the sliders within 1 common class
+		System.out.println("Number of sliders:\s"+sliders.size());
+		
+		
+		
+		
+		
 	}
 
 }
