6007884e3a692dc8dd6878a71c1c2d7110e3b9e7
daniela <daniela_056@hotmail.com>
2022-02-28 19:18:36 -0600

    Relative path

---
+++ src/day22/XPathDemo.java
@@ -0,0 +1,27 @@
+package day22;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XPathDemo {
+
+	public static void main(String[] args) 
+	{
+		System.setProperty("webdriver.chromedriver", "/usr/local/bin/chromedriver");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("http://automationpractice.com/index.php");
+		driver.manage().window().maximize();
+		
+		//Absolute xpath
+		//driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/header[1]/div[3]/div[1]/div[1]/div[2]/form[1]/input[4]")).sendKeys("T-Shirts");
+		//driver.findElement(By.xpath("/html[1]/body[1]/div[1]/div[1]/header[1]/div[3]/div[1]/div[1]/div[2]/form[1]/button[1]")).click();
+		
+		//Relative xpath
+		driver.findElement(By.xpath("//input[@id='search_query_top']")).sendKeys("T-shirts");
+		driver.findElement(By.xpath("//button[@name='submit_search']")).click();
+
+	}
+
+}
