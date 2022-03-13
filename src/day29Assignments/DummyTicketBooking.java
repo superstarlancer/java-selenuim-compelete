9badf2101aa32d14f1b8fe0e690be188e57e4524
daniela <daniela_056@hotmail.com>
2022-03-13 17:50:45 -0500

    Dummy tickets

--- src/day29Assignments/DummyTicketBooking.java
+++ src/day29Assignments/DummyTicketBooking.java
@@ -35,9 +35,39 @@ public class DummyTicketBooking {
 		driver.findElement(By.xpath("//input[@id='addmorepax']")).click();
 		driver.findElement(By.id("traveltype_2")).click();
 		
+		driver.findElement(By.name("fromcity")).sendKeys("Hyderabad"); //From city / Origin
+		driver.findElement(By.name("tocity")).sendKeys("Delhi"); //To city. /Destination 
 		
+		driver.findElement(By.xpath("//input[@id='departon']")).sendKeys("10/03/2022"+Keys.ENTER); //Departure date  DD/MM/YYYY
+		driver.findElement(By.xpath("//input[@id='returndate']")).sendKeys("15/03/2022"+Keys.ENTER); //Return date DD/MM/YYYY
 		
+		//Delivery options
+		driver.findElement(By.xpath("//span[@id='select2-reasondummy-container']")).click();
+		driver.findElement(By.xpath("//input[@role='combobox']")).sendKeys("Visa Extension"+Keys.ENTER);
 		
+		//How will you like to receive the dummy ticket (optional)
+		driver.findElement(By.xpath("//input[@id='deliverymethod_2']")).click(); //WhatsApp
+				
+					
+		//Billing Details:
+	
+		driver.findElement(By.name("billname")).sendKeys("John");
+		driver.findElement(By.name("billing_phone")).sendKeys("703-789-5417");
+		driver.findElement(By.name("billing_email")).sendKeys("abcd@gmail.com");
+		
+		driver.findElement(By.xpath("//span[@id='select2-billing_country-container']")).click(); //Boostrap DropDown
+		driver.findElement(By.xpath("//input[@role='combobox']")).sendKeys("India"+Keys.ENTER); //Country
+		
+		//Street address 
+		driver.findElement(By.name("billing_address_1")).sendKeys("13646 ABC");
+		driver.findElement(By.name("billing_address_2")).sendKeys("ABC");
+		driver.findElement(By.name("billing_city")).sendKeys("HYD");
+		
+		//State selection 
+		driver.findElement(By.xpath("//span[@id='select2-billing_state-container']")).click(); //Boostrap DropDown
+		driver.findElement(By.xpath("//input[@role='combobox']")).sendKeys("Telangana"+Keys.ENTER); //state
+				
+		driver.findElement(By.id("billing_postcode")).sendKeys("500090"); //Pin
 		
 		
 		
