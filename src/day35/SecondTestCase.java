7f22c907a5134e7f3bf28507d17a222cd50331e5
daniela <daniela_056@hotmail.com>
2022-03-16 11:22:45 -0500

    Passed test suite

--- src/day35/SecondTestCase.java
+++ src/day35/SecondTestCase.java
@@ -12,20 +12,20 @@ import org.testng.annotations.Test;
 public class SecondTestCase 
 {
 	
-	WebDriver driver; //global variable , defining type and instantiating
-	
+	WebDriver driver;
+
 	@Test(priority=1)
-	void openApp() 
+	void openApp()
 	{
-		System.setProperty("webdriver.chromedriver", "/usr/local/bin/chromedriver");
+		System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 		driver=new ChromeDriver();
-		driver.get(" https://demo.nopcommerce.com/login");
-	} 
+		driver.get("https://admin-demo.nopcommerce.com/login");
+	}
 	
-	@Test(priority=3)
-	void Login() 
+	@Test(priority=2)
+	void login()
 	{
-		WebElement emailtxt=driver.findElement(By.xpath("//inout[@id='Email']"));
+		WebElement emailtxt=driver.findElement(By.xpath("//input[@id='Email']"));
 		emailtxt.clear();
 		emailtxt.sendKeys("admin@yourstore.com");
 		
@@ -34,28 +34,27 @@ public class SecondTestCase
 		passtxt.sendKeys("admin");
 		
 		driver.findElement(By.xpath("//button[normalize-space()='Log in']")).click();
+		
 		String exp_lbl=driver.findElement(By.xpath("//h1[normalize-space()='Dashboard']")).getText();
 		
-		
-		/*if(exp_lbl.equals("Dqashboard"))
+		/*if(exp_lbl.equals("Dashboards"))
 		{
 			System.out.println("test passed");
+						
 		}
-		else 
+		else
 		{
 			System.out.println("test failed");
 		}*/
 		
-		//assertions
-		Assert.assertEquals(exp_lbl, "Dashboard");
-		
-		
-	}  
+		Assert.assertEquals(exp_lbl, "Dashboard","Labels are not equal..");
 	
-	@Test(priority=2)
-	void closeApp() 
+	
+	}
+	
+	@Test(priority=3)
+	void closeApp()
 	{
 		driver.close();
-	}  
-  
+	}
 }
